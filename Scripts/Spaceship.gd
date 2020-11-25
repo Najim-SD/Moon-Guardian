f18e4c0f9224d7dfd3394f964db7a0f544432633
Epsilon <dev.djvan@gmail.com>
2020-11-25 18:22:53 +0200

    Smooth spaceship movement

--- Scripts/Spaceship.gd
+++ Scripts/Spaceship.gd
@@ -7,7 +7,13 @@ extends KinematicBody2D
 
 var velocity = Vector2()
 var direction = Vector2()
-var speed = 500.0
+var accelration = 25
+var power = 0.0
+var friction = 0.03
+var maxSpeed = 500.0
+
+export var useJoyStick = true
+export var controlDevice = 0
 
 
 # Called when the node enters the scene tree for the first time.
@@ -17,13 +23,53 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta):
-	direction = (get_global_mouse_position() - global_position).normalized()
+	if useJoyStick == false:
+		direction = (get_global_mouse_position() - global_position).normalized()
+		look_at(get_global_mouse_position())
+	else:
+		var nd = Vector2(Input.get_joy_axis(0, 0), Input.get_joy_axis(0, 1))
+		if(nd.length() > 0.3):
+			direction = vectorLerp(direction, nd, 0.1)
+			rotation = direction.angle()
 	
-	velocity = direction * speed
-	look_at(get_global_mouse_position())
-	if Input.is_action_pressed("move"):
-		velocity = move_and_slide(velocity, Vector2.UP)
+	if isPressed("move"):
+		power = min(accelration/2 + power, maxSpeed)
+		velocity = vectorLerp(velocity, (direction * power), 0.1)
 		get_node("JetFlame").show()
 	else:
 		get_node("JetFlame").hide()
-	pass	#_Physics Process
+		power = lerp(power, 0, friction)
+		velocity = vecFriction(velocity)
+	
+	velocity = move_and_slide(velocity, Vector2.UP)
+
+func getDist(pos1:Vector2, pos2:Vector2):
+	return abs(pos1.x - pos2.x) + abs(pos1.y - pos2.y)
+	
+
+func vecFriction(vec:Vector2):
+	vec.x = lerp(vec.x, 0, friction)
+	vec.y = lerp(vec.y, 0, friction)
+	return vec
+
+func vectorLerp(vec:Vector2, to:Vector2, f:float):
+	vec.x = lerp(vec.x, to.x, f)
+	vec.y = lerp(vec.y, to.y, f)
+	return vec
+	
+
+func isPressed(key:String):
+	if useJoyStick == false:
+		if key == "move" and getDist(get_global_mouse_position(), position) < 70: return false
+		if Input.is_action_pressed(key):
+			return true
+		else : return false
+	if Input.is_action_pressed(key+String(controlDevice)):
+		return true
+	else: return false
+
+
+
+
+
+
